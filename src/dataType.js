6495117a6f226abb885d96aff1cabcf135cf73a6
shipeifei <shipeifei_gonghe@163.com>
2015-11-16 13:47:47 +0800

    修改了数据类型文件，增加了数据类型的转换

--- src/dataType.js
+++ src/dataType.js
@@ -8,25 +8,58 @@ var dataType = dataType || {};
 (function() {
 
 
-	var self = dataType;
+    var self = dataType;
 
-	self.cs = {
-		//数据库字段类型
-		pk: 'pk',
-		text: 'text',
-		integer: 'integer',
-		numeric: 'numeric',
-		date: 'date',
-		bool: 'boolean',
-		//数据库查询类型
-		rowset: 'rowset',
-		row: 'row',
-		scalar: 'scalar',
-		nonQuery: 'non-query',
-		insert: 'insert',
-		update:'update',
-		any: 'any'
-	};
+    self.cs = {
+        //数据库字段类型
+        pk: 'pk',
+        text: 'text',
+        integer: 'int',
+        numeric: 'numeric',
+        decimal:'decimal',
+        date: 'date',
+        bool: 'boolean',
+
+        //数据库查询类型
+        rowset: 'rowset',
+        row: 'row',
+        scalar: 'scalar',
+        nonQuery: 'non-query',
+        insert: 'insert',
+        update: 'update',
+        any: 'any'
+    };
+
+    /**
+     * 转换数据类型指定为sqlite特定的属性
+     * @param  {[type]} typeName [description]
+     * @return {[type]}          [description]
+     */
+    self.translateType = function(typeName) {
+        var _result = typeName;
+
+        switch (typeName) {
+            case "pk":
+                _result = "INTEGER PRIMARY KEY  AUTOINCREMENT";
+                break;
+            case "int":
+                _result = "INTEGER";
+                break;
+            case "decimal":
+                _result = "numeric";
+                break;
+            case "date":
+                _result = "datetime";
+                break;
+            case "text":
+                _result = "text";
+                break;
+            case "boolean":
+                _result = "boolean";
+                break;
+        }
+        return _result;
+    };
 
 
 
